d8b2c9314f633f5511ce2f84d547378c0f2c6676
RENJITHPS10 <renjithpstech@gmail.com>
2024-09-04 21:39:39 +0530

    bug fixed

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const search = async () => {
     if (result.value) {
         let response = await fetch(`https://restcountries.com/v3.1/name/${result.value}?fullText=true`)
         response.json().then(data => {
-            let country = data[0];
-            let flag = country.flags.png;
-            let name = country.name.common;
-            let population = country.population.toLocaleString();
+            let country = data[0]
+            let flag = country.flags.png
+            let name = country.name.common
+            let population = country.population
             let capital = country.capital
-            let region = country.region;
+            let region = country.region
             let area = country.area
             let border = country.borders
             let language = country.languages
@@ -34,10 +34,10 @@ const search = async () => {
                                 <li class="list-group-item bg-transparent text-white"><strong>Population:</strong> ${population}</li>
                                 <li class="list-group-item bg-transparent text-white"><strong>Capital:</strong> ${capital}</li>
                                 <li class="list-group-item bg-transparent text-white"><strong>Region:</strong> ${region}</li>
-                                <li class="list-group-item bg-transparent text-white"><strong>Borders:</strong> ${border ? border.join(', ') : 'N/A'}</li>
+                                <li class="list-group-item bg-transparent text-white"><strong>Borders:</strong> ${border}</li>
                                 <li class="list-group-item bg-transparent text-white"><strong>Time Zone:</strong> ${time}</li>
                                 <li class="list-group-item bg-transparent text-white"><strong>Area (km²):</strong> ${area}</li>
-                                <li class="list-group-item bg-transparent text-white"><strong>Languages:</strong> ${national_language.join(', ')}</li>
+                                <li class="list-group-item bg-transparent text-white"><strong>Languages:</strong> ${national_language}</li>
                                 <li class="list-group-item bg-transparent text-white"><strong>Currencies:</strong> ${currencysymbol} ${currencyname}</li>
                                 <li class="list-group-item bg-transparent text-white"><strong>Map:</strong> <a href="${map}" class="link-success" target="_blank"
                                 >Google Map</a></li>
